284ec957b9d4de2d00e700fbfbb7bad9e720568c
Bryan <fragment2501@users.noreply.github.com>
2021-10-13 15:53:18 -0400

    initial commit. Default webapi template.

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace bsr_dotnet_ef6_webapi
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
